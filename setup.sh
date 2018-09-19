84ac96c5d5f889a966ee45cc763bd982dbd6d768
Msch0150 <37273467+Msch0150@users.noreply.github.com>
2018-09-19 20:57:32 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,6 +4,7 @@
 mkdir -p docker-solr-data/nomisma/data
 mkdir -p docker-solr-data/nomisma-fuseki/data
 mkdir -p docker-nomisma-data/nomisma-data/id
+mkdir -p docker-nomisma-data/nomisma-data/ontology
 mkdir -p docker-nomisma-data/nomisma
 #mkdir -p docker-loris-data/images
 #mkdir -p loris
