af5d1189200e5146ae5b9c6aac8f2291031c49b0
Msch0150 <37273467+Msch0150@users.noreply.github.com>
2018-09-19 07:22:51 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-mkdir -p docker-exist-data
-mkdir -p docker-solr-data/numishare-published/data
-mkdir -p docker-loris-data/images
-mkdir -p loris
+#mkdir -p docker-exist-data
+mkdir -p docker-solr-data/nomisma/data
+mkdir -p docker-solr-data/nomisma-fuseki/data
+#mkdir -p docker-loris-data/images
+#mkdir -p loris
 
 chown -R 8983:8983 docker-solr-data
 
